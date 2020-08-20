defab67c32e10a8ecf97b5515193eac1ad08d6d9
“alejandra-gutierrez” <ag1917@ic.ac.uk>
2020-08-20 12:30:29 +0100

    Spotify - with data analysis

--- venv/feature_analysis.py
+++ venv/feature_analysis.py
@@ -6,43 +6,164 @@ from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-
-features = ['energy', 'liveness', 'tempo', 'speechiness', 'acousticness', 'instrumentalness', 'time_signature', 'danceability', 'key', 'loudness', 'valence']
-df = pd.read_csv("audio_features.csv")
-# print(tabulate(df[50:100], headers='keys', tablefmt='psql'))
-
-x = df.loc[:, features].values
-y = df.loc[:,['uri']].values
-
-x = StandardScaler().fit_transform(x)
-
-pca = PCA(n_components=2)
-pca.fit(x)
-principalComponents = pca.fit_transform(x)
-principal_df = pd.DataFrame(data = principalComponents, columns = ['PC1', 'PC2'])
+import plotly.express as px
+from sklearn import preprocessing
+from sklearn.cluster import KMeans
+from collections import Counter
+from wordcloud import WordCloud, STOPWORDS
+from sklearn.metrics import silhouette_samples, silhouette_score
+from yellowbrick.cluster import SilhouetteVisualizer
+from yellowbrick.datasets import load_nfl
 
 
-# ax = sns.scatterplot(x=principal_df['PC1'], y=principal_df['PC2'], data=principal_df)
+df = pd.read_csv("final_audio_features.csv")
+df1 = pd.read_csv("final_audio_features.csv")
+df1.drop(['duration_ms', 'uri', 'genre', 'type', 'mode', 'key'], axis=1, inplace=True)
+
+
+#  ----------------------------------------- Scaled Data  ---------------------
+x = df1.values #returns a numpy array
+min_max_scaler = preprocessing.MinMaxScaler()
+x_scaled1 = min_max_scaler.fit_transform(x)
+df1 = pd.DataFrame(x_scaled1)
+df1.columns = ["energy", "liveness", "tempo", "speechiness", "acousticness", "instrumentalness", "time_signature", "danceability", "loudness",'valence' ]
+
+
+# --------------------------------- Violing plot and heat map --------------
+
+# ax = sns.heatmap(df1.cov(),vmin=-0.08, vmax=0.08, annot = True)
 # plt.show()
-# print(pca.explained_variance_ratio_)
-# print(pca.components_)
-# print(pca.explained_variance_)
+
+# df2 = df1.melt(var_name='groups', value_name='vals')
+# ax = sns.violinplot(x="groups", y="vals", data=df2, linewidth = 0.6, inner = 'point', scale= 'width')
+# plt.show()
 
 
-def draw_vector(v0, v1, ax=None):
-    ax = ax or plt.gca()
-    arrowprops=dict(arrowstyle='->',
-                    linewidth=2,
-                    shrinkA=0, shrinkB=0)
-    ax.annotate('', v1, v0, arrowprops=arrowprops)
-# plot data
-plt.scatter(principal_df['PC1'], principal_df['PC2'], alpha=0.2)
-for length, vector in zip(pca.explained_variance_, pca.components_):
-    v = vector * 3 * np.sqrt(length)
+#  -------------------------------- Finding k  -------------------------------
 
-    draw_vector(pca.mean_, pca.mean_ + v)
+df1.drop(['liveness', 'speechiness', 'instrumentalness', 'loudness', 'time_signature'], axis=1, inplace=True)
+
+x = df1.values #returns a numpy array
+x_scaled2 = min_max_scaler.fit_transform(x)
+df1 = pd.DataFrame(x_scaled2)
+
+distortions = []
+for i in range(1, 11):
+    km = KMeans(
+        n_clusters=i, init='random',
+        n_init=10, max_iter=300,
+        tol=1e-04, random_state=0
+    )
+    km.fit(x_scaled2)
+    distortions.append(km.inertia_)
+
+# plt.plot(range(1, 11), distortions, marker='o')
+# plt.xlabel('Number of clusters')
+# plt.ylabel('Distortion')
+# plt.show()
+
+#  ------------------------ k-means Clustering ---------------------
+
+kmeans = KMeans(init="k-means++",
+                n_clusters=3,
+                random_state=15,
+                max_iter = 500).fit(x_scaled2)
+df1['kmeans'] = kmeans.labels_
+df1.columns = ['energy', 'tempo', 'acousticness','danceability','valence', 'kmeans']
+
+grouped = df1.groupby(['kmeans']).mean()
+kmeans = df1['kmeans']
+df['kmeans'] = kmeans
+grouped = df.groupby(['kmeans']).count()
 
 
+# ---------------------------------- Violin Plot for each cluster ------------------
+c0 = df[df['kmeans']==0]
+c1 = df[df['kmeans']==1]
+c2 = df[df['kmeans']==2]
+c0.drop(['kmeans','duration_ms', 'uri', 'genre', 'type', 'mode', 'key', 'liveness', 'speechiness', 'instrumentalness', 'time_signature', 'loudness'], axis=1, inplace=True)
+c1.drop(['kmeans','duration_ms', 'uri', 'genre', 'type', 'mode', 'key', 'liveness', 'speechiness', 'instrumentalness', 'time_signature', 'loudness'], axis=1, inplace=True)
+c2.drop(['kmeans','duration_ms', 'uri', 'genre', 'type', 'mode', 'key', 'liveness', 'speechiness', 'instrumentalness', 'time_signature', 'loudness'], axis=1, inplace=True)
 
-plt.axis('equal');
-plt.show()
+x = c0.values #returns a numpy array
+min_max_scaler = preprocessing.MinMaxScaler()
+c0_scaled = min_max_scaler.fit_transform(x)
+c0 = pd.DataFrame(c0_scaled)
+c0.columns = ["energy", "tempo", "acousticness","danceability", 'valence' ]
+c0=c0.melt(var_name='groups', value_name='vals')
+
+x = c1.values #returns a numpy array
+min_max_scaler = preprocessing.MinMaxScaler()
+c1_scaled = min_max_scaler.fit_transform(x)
+c1 = pd.DataFrame(c1_scaled)
+c1.columns = ["energy", "tempo", "acousticness","danceability", 'valence' ]
+c1=c1.melt(var_name='groups', value_name='vals')
+
+x = c2.values #returns a numpy array
+min_max_scaler = preprocessing.MinMaxScaler()
+c2_scaled = min_max_scaler.fit_transform(x)
+c2 = pd.DataFrame(c2_scaled)
+c2.columns = ["energy", "tempo", "acousticness","danceability", 'valence' ]
+c2=c2.melt(var_name='groups', value_name='vals')
+
+# f, axes = plt.subplots(3, 1)
+# ax = sns.violinplot( data=c1 ,x="groups", y="vals", linewidth = 0.6, inner = 'point', scale= 'width', ax=axes[0])
+# ax = sns.violinplot( data=c2 ,x="groups", y="vals", linewidth = 0.6, inner = 'point', scale= 'width', ax=axes[1])
+# ax = sns.violinplot( data=c0 ,x="groups", y="vals", linewidth = 0.6, inner = 'point', scale= 'width', ax=axes[2])
+# plt.show()
+
+# ---------------------------------- Genre -------------------------------------
+
+d0 = df[df['kmeans']==0]
+d1 = df[df['kmeans']==1]
+d2 = df[df['kmeans']==2]
+
+d1 = d1.astype(str)
+d0 = d0.astype(str)
+d2 = d2.astype(str)
+
+words = ''
+for i in d1.genre.values:
+    words += '{} '.format(i.lower()) # save all words in a string
+
+wd = pd.DataFrame(Counter(words.split()).most_common(20), columns=['word', 'frequency'])
+data1 = dict(zip(wd['word'].tolist(), wd['frequency'].tolist()))
+
+words = ''
+for i in d2.genre.values:
+    words += '{} '.format(i.lower()) # save all words in a string
+wd = pd.DataFrame(Counter(words.split()).most_common(20), columns=['word', 'frequency'])
+data2 = dict(zip(wd['word'].tolist(), wd['frequency'].tolist()))
+
+words = ''
+for i in d0.genre.values:
+    words += '{} '.format(i.lower()) # save all words in a string
+wd = pd.DataFrame(Counter(words.split()).most_common(20), columns=['word', 'frequency'])
+data0 = dict(zip(wd['word'].tolist(), wd['frequency'].tolist()))
+
+wc = WordCloud(background_color='white',
+                width=800,
+                height=400,
+                max_words=100).generate_from_frequencies(data0)
+plt.figure(figsize=(10, 10))
+plt.imshow(wc, interpolation='bilinear')
+plt.axis('off')
+# plt.show()
+
+wc = WordCloud(background_color='white',
+                width=800,
+                height=400,
+                max_words=100).generate_from_frequencies(data1)
+plt.figure(figsize=(10, 10))
+plt.imshow(wc, interpolation='bilinear')
+plt.axis('off')
+# plt.show()
+
+wc = WordCloud(background_color='white',
+                width=800,
+                height=400,
+                max_words=100).generate_from_frequencies(data2)
+plt.figure(figsize=(10, 10))
+plt.imshow(wc, interpolation='bilinear')
+plt.axis('off')
+# plt.show()
--- venv/get_data.py
+++ venv/get_data.py
@@ -17,7 +17,6 @@ from spotipy.oauth2 import SpotifyOAuth
 from tabulate import tabulate
 import spotipy
 
-
 scope = "user-library-read playlist-modify-private"
 
 OAuth = SpotifyOAuth(scope=scope,
@@ -27,75 +26,89 @@ OAuth = SpotifyOAuth(scope=scope,
         username= 'alejandra.vlerick')
 sp = spotipy.Spotify(auth_manager=OAuth)
 
-results = sp.current_user_saved_tracks()
-for idx, item in enumerate(results['items']):
-    track = item['track']
+# results = sp.current_user_saved_tracks()
+# for idx, item in enumerate(results['items']):
+#     track = item['track']
     # print(idx, track['artists'][0]['name'], " – ", track['name'])
 # ----------------------------------
 
-def get_playlist_content():
-    offset = 0
-    songs = []
-    while True:
-        content = sp.current_user_saved_tracks(limit=50, offset=offset)
-        songs += content['items']
-        if content['next'] is not None:
-            offset += 50
-        else:
-            break
-    #
-    # with open('saved_tracks', 'w') as outfile:
-    #     json.dump(songs, outfile)
+# def get_playlist_content():
+#     offset = 0
+#     songs = []
+#     while True:
+#         content = sp.current_user_saved_tracks(limit=50, offset=offset)
+#         songs += content['items']
+#         if content['next'] is not None:
+#             offset += 50
+#         else:
+#             break
+#     #
+#     # with open('saved_tracks', 'w') as outfile:
+#     #     json.dump(songs, outfile)
+#
+# def get_playlist_audio_features():
+#     offset = 0
+#     songs = []
+#     items = []
+#     ids = []
+#     while True:
+#         content = sp.current_user_saved_tracks(limit=50, offset=offset)
 
-def get_playlist_audio_features():
-    offset = 0
-    songs = []
-    items = []
-    ids = []
-    while True:
-        content = sp.current_user_saved_tracks(limit=50, offset=offset)
-        songs += content['items']
-        if content['next'] is not None:
-            offset += 100
-        else:
-            break
-
-    for i in songs:
-        ids.append(i['track']['id'])
-
-    index = 0
-    audio_features = []
-    while index < len(ids):
-        audio_features += sp.audio_features(ids[index:index + 50])
-        index += 50
-    features_list = []
-    for features in audio_features:
-        features_list.append([features['energy'], features['liveness'],
-                              features['tempo'], features['speechiness'],
-                              features['acousticness'], features['instrumentalness'],
-                              features['time_signature'], features['danceability'],
-                              features['key'], features['duration_ms'],
-                              features['loudness'], features['valence'],
-                              features['mode'], features['type'],
-                              features['uri']])
-
-    df = pd.DataFrame(features_list, columns=['energy', 'liveness',
-                                              'tempo', 'speechiness',
-                                              'acousticness', 'instrumentalness',
-                                              'time_signature', 'danceability',
-                                              'key', 'duration_ms', 'loudness',
-                                              'valence', 'mode', 'type', 'uri'])
-    print(tabulate(df,headers='firstrow'))
-    # df.to_csv('audio_features.csv', index=False)
-    print(df.columns)
-
-
-# get_playlist_content()
-# get_playlist_audio_features()
-df = pd.read_csv("audio_features.csv")
-print(tabulate(df[50:100], headers='keys', tablefmt='psql'))
+#         songs += content['items']
+#         if content['next'] is not None:
+#             offset += 100
+#         else:
+#             break
+#
+#     for i in songs:
+#         ids.append(i['track']['id'])
+#
+#     index = 0
+#     audio_features = []
+#     while index < len(ids):
+#         audio_features += sp.audio_features(ids[index:index + 50])
+#         index += 50
+#     features_list = []
+#     for features in audio_features:
+#         features_list.append([features['energy'], features['liveness'],
+#                               features['tempo'], features['speechiness'],
+#                               features['acousticness'], features['instrumentalness'],
+#                               features['time_signature'], features['danceability'],
+#                               features['key'], features['duration_ms'],
+#                               features['loudness'], features['valence'],
+#                               features['mode'], features['type'],
+#                               features['uri']])
+#
+#     df = pd.DataFrame(features_list, columns=['energy', 'liveness',
+#                                               'tempo', 'speechiness',
+#                                               'acousticness', 'instrumentalness',
+#                                               'time_signature', 'danceability',
+#                                               'key', 'duration_ms', 'loudness',
+#                                               'valence', 'mode', 'type', 'uri'])
+#     print(tabulate(df,headers='firstrow'))
+#     # df.to_csv('audio_features.csv', index=False)
+#     print(df.columns)
 
 
-# get artist genre
-# https://developer.spotify.com/documentation/web-api/reference/artists/get-artist/
-# merge back into saved songs dataset
+# df = pd.read_csv("audio_features.csv")
+# df["genre"] = ""
+# print(tabulate(df[50:100], headers='keys', tablefmt='psql'))
+#
+# for index, row in df.iterrows():
+#         uri = row['uri']
+#         track = sp.track(uri)
+#         artist_uri = track['album']['artists'][0]['uri']
+#         artist = sp.artist(artist_uri)
+#
+#         genres = artist['genres']
+#         str_genres = ' '.join([str(elem) for elem in genres])
+#         row['genre'] = str_genres
+#         df.loc[index, 'genre'] = row['genre']
+#         # print(df.loc[index,'genre'])
+#
+#
+# print(tabulate(df[50:100], headers='keys', tablefmt='psql'))
+# df.to_csv('final_audio_features.csv', index=False)
+
+playlist = sp.current_user_playlists()
+print(playlist['items'][1]['id'])
